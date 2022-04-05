6f2566884ac027d3108bef4686e36bfa383fb36a
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-04-05 22:50:02 +0600

    error page update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
 import Review from './Components/Review/Review';
+import ReviewTwo from './Components/ReviewTow/ReviewTwo';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/reviews" element={<Review></Review>}></Route>
+        <Route path="/reviewtwo" element={<ReviewTwo></ReviewTwo>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blogs" element={<Blog></Blog>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
